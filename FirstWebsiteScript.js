236d561c32fd825e478b472a8781ef77bd805628
blooopbleeep <78101567+blooopbleeep@users.noreply.github.com>
2021-03-25 13:55:25 -0700

    Update FirstWebsiteScript.js

--- FirstWebsiteScript.js
+++ FirstWebsiteScript.js
@@ -6,7 +6,12 @@
         frameRate(30);
         
         // ProgramCodeGoesHere
-        //my drawin stuff hahahahahhah
+        draw = function() {
+        strokeWeight(2);
+        stroke(255, 255, 255);
+        fill(78, mouseX, 140);
+        image(getImage("https://www.mariowiki.com/images/0/03/PMCS_MarioSprite.png"), mouseX, mouseY, 200,200);
+           };
         }
      };
 
