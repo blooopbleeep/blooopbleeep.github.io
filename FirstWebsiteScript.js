957872d46948108f0c11f5ffb3152a03ed21370e
blooopbleeep <78101567+blooopbleeep@users.noreply.github.com>
2021-03-25 13:15:24 -0700

    Delete FirstWebsiteScript.js

--- FirstWebsiteScript.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-
-draw = function() {
-    strokeWeight(2);
-    stroke(255, 255, 255);
-    fill(78, mouseX, 140);
-    image(getImage("https://www.mariowiki.com/images/0/03/PMCS_MarioSprite.png"), mouseX, mouseY, 200,200);
-    
-};
