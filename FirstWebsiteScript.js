7c719d77935c893949d6bab529bc3d9e7305a9a4
blooopbleeep <78101567+blooopbleeep@users.noreply.github.com>
2021-03-25 13:13:20 -0700

    Update FirstWebsiteScript.js

--- FirstWebsiteScript.js
+++ FirstWebsiteScript.js
@@ -6,6 +6,6 @@ draw = function() {
     strokeWeight(2);
     stroke(255, 255, 255);
     fill(78, mouseX, 140);
-    image(var myElement = document.getElementById("mariopic"), mouseX, mouseY, 200,200);
+    image(getImage("https://www.mariowiki.com/images/0/03/PMCS_MarioSprite.png"), mouseX, mouseY, 200,200);
     
 };
