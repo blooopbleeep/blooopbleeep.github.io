a37dd44558d238c79cf5d3cc1c482d64bb3b6d2a
blooopbleeep <78101567+blooopbleeep@users.noreply.github.com>
2021-03-25 13:46:28 -0700

    Update FirstWebsiteScript.js

--- FirstWebsiteScript.js
+++ FirstWebsiteScript.js
@@ -1,11 +1,18 @@
 // JavaScript source code
 
-//hi guys
+ var sketchProc = function(processingInstance) {
+     with (processingInstance) {
+        size(400, 400); 
+        frameRate(30);
+        
+        // ProgramCodeGoesHere
+        //my drawin stuff hahahahahhah
+        }
+     };
 
-draw = function() {
-    strokeWeight(2);
-    stroke(255, 255, 255);
-    fill(78, mouseX, 140);
-    image(getImage("https://www.mariowiki.com/images/0/03/PMCS_MarioSprite.png"), mouseX, mouseY, 200,200);
+    // Get the canvas that Processing-js will use
+    var canvas = document.getElementById("mycanvas"); 
+    // Pass the function sketchProc (defined in myCode.js) to Processing's constructor.
+    var processingInstance = new Processing(canvas, sketchProc);
+
     
-};
